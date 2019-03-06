9045d629b743b755a6e910b55c6314fab8e5c8be
notjamesobrien <jtwebco@gmail.com>
2019-03-06 11:36:56 -0500

    animate engagement rate count up

--- src/components/shared/engagement-box/EngagementBox.js
+++ src/components/shared/engagement-box/EngagementBox.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import { observer } from 'mobx-react'
+import CountUp from 'react-countup'
 
-const EngagementBox = observer(props => (
-  <div className='engagement-box'>
+const EngagementBox = observer(props => {
+  return <div className='engagement-box'>
     <ul className='engagement-box__grid'>
       <li className='engagement-box__grid-item'>
         <p className='text text--x-small text--white'>
           Engagement Rate
         </p>
-        <p className='engagement-box__rate'>{props.rates}%</p>
+        <p className='engagement-box__rate'>
+          <CountUp end={props.rates} decimals={2} decimal='.' />%
+        </p>
       </li>
       <li className='engagement-box__grid-item'>
         <p className='text text--x-small text--white'>
           F/F Ratio
         </p>
-        <p className='engagement-box__rate'>{props.ratio}</p>
+        <p className='engagement-box__rate'>
+          {props.ratio}
+        </p>
       </li>
       <li className='engagement-box__grid-item'>
         <p className='text text--x-small text--white'>
@@ -30,6 +35,6 @@ const EngagementBox = observer(props => (
       </li>
     </ul>
   </div>
-))
+})
 
 export default EngagementBox
