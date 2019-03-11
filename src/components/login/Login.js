43f91f0397a8762d2bae29174a364f39e4350e48
notjamesobrien <jtwebco@gmail.com>
2019-03-11 15:37:28 -0400

    more mobile fixes

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -6,13 +6,15 @@ const Login = () => (
   <div className='login'>
     <h1 className='login__logo'>gage</h1>
     <h2 className='login__tagline text text--small text--white'>
-      your social engagement stats
+      instant engagement insights
     </h2>
     <button onClick={store.handleLogin}
             className='login__button'
             title='Login with Instagram'>
       <i className='fab fa-instagram' />
-      <p className='text text--medium text--white'>Get Started</p>
+      <p className='text text--medium-small text--white'>
+        Login with Instagram
+      </p>
     </button>
     <div className='login__speedometer' />
     <Footer />
